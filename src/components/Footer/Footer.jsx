d47c45b8b812f7fdc64b74df472e749f7968dd1c
NusfaJahan61 <nusfaikra@gmail.com>
2025-11-30 23:59:31 +0600

    Footer v2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,52 +1,48 @@
+import { FaFacebook } from "react-icons/fa";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { FaTelegram } from "react-icons/fa";
+import { assets } from "../../assets/assets";
+
 const Footer = () => {
   return (
     <div class=" capitalize bg-[#D69604] flex flex-col  h-120 *:text-black ">
       <div class="w-3/4 m-auto mb-10 mt-20 flex justify-between">
         <div class="">
-          <h1 class="font-bold uppercase text-[20px] tracking-widest">Communicate</h1>
-          <button class="hover:bg-[#9EAAA1] px-10 py-2 hover:text-zinc-900 rounded-md my-5 border border-[#B5C3BD] active:bg-[#B5C3BD] bg-transparent text-zinc-100 duration-400">send e-mail</button>
-          <p><a href="#" class="hover:underline duration-500 font-bold">Sign up</a> to receive special offers!</p>
-          <div class="flex pt-5 gap-5 *:rounded-full">
-            <div class="size-10 bg-zinc-200"></div>
-            <div class="size-10 bg-zinc-200"></div>
-            <div class="size-10 bg-zinc-200"></div>
+          <img class="w-60 -ml-6" src={assets.logo} alt="" />
+          <button class=" px-9 py-2 text-zinc-900 rounded-md my-4 border border-black hover:border-transparent hover:bg-amber-300 active:bg-[#f5df1d] bg-transparent  duration-400 hover:cursor-pointer">send email</button>
+          <div class="flex pt-5 gap-5 *:text-4xl *:hover:cursor-pointer">
+            <FaFacebook />
+            <IoLogoWhatsapp />
+            <FaTelegram />
           </div>
         </div>
         <div class="flex gap-10">
-          <div class="w-50 flex flex-col gap-5">
-            <h1 class="font-bold uppercase text-[18px] tracking-widest">contact us</h1>
-            <p class="text-[14px]">DFO Office, Sundarbans East Forest Division, Khulna, Bangladesh
+          <div class="w-60 flex flex-col gap-5 *:hover:cursor-pointer">
+            <h1 class="font-bold uppercase text-[18px] tracking-widest ">contact us</h1>
+            <p class="text-[15px]">DFO Office, Sundarbans East Forest Division, Khulna, Bangladesh
             </p>
             <p>+44 20 7722 0088</p>
             <p>honeybee@gmail.com</p>
           </div>
-          <div class="w-50 flex flex-col gap-1">
-            <h1 class="font-bold uppercase text-[18px] tracking-widest">category</h1>
-            <p>decor</p>
-            <p>furniture</p>
-            <p>lighting</p>
-            <p>interior design</p>
-            <p>Arcticture design</p>
-            <p>commercial design</p>
+          <div class="w-50 flex flex-col gap-3 *:hover:cursor-pointer">
+            <h1 class="font-bold uppercase text-[18px] tracking-widest mb-2">category</h1>
+            <p>Litchi Flower Honey</p>
+            <p>Mustard Flower Honey</p>
+            <p>Sundarban Honey</p>
+            <p>Black Seed Honey </p>
           </div>
-          <div class="w-50 flex flex-col gap-1">
-            <h1 class="font-bold uppercase text-[18px] tracking-widest">company</h1>
+          <div class="w-50 flex flex-col gap-3 *:hover:cursor-pointer">
+            <h1 class="font-bold uppercase text-[18px] tracking-widest mb-2">company</h1>
             <p>about us</p>
             <p>services</p>
             <p>contact us</p>
-            <p>portfolio</p>
-            <p>delevery</p>
-            <p>blog</p>
+            <p>delivery</p>
           </div>
         </div>
       </div>
       <hr class="w-3/4 m-auto my-0" />
-      <div class="w-3/4 m-auto my-5 flex justify-between">
-        <div class="block text-4xl font-bold text-[#F9FDFB]">Luxurio</div>
-        <div class="">
-          <a class="block" href="#">Privacy policy</a>
-          <a class="" href="#">Terms and conditions</a>
-        </div>
+      <div class="w-3/4 m-auto my-5  justify-center">
+        <p>Privacy policy All rights reserved.</p>
       </div>
     </div>
   )
