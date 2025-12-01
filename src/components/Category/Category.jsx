5e8e18b1484b8748d0f4d7726a828122672e2eaa
NusfaJahan61 <nusfaikra@gmail.com>
2025-12-01 23:59:33 +0600

    Category v2

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -2,16 +2,34 @@ import { assets } from '../../assets/assets'
 
 const Category = () => {
   return (
-    <div >
-      <ul className="flex justify-evenly">
-        <li className=''><img src={assets.nuts} className='w-20 h-20' alt="all" /><p>All</p></li>
-        <li><img src={assets.nuts} className='w-20 h-20' alt="honey" /><p>Honey</p></li>
-        <li><img src={assets.nuts} className='w-20 h-20' alt="nuts" /><p>Nuts</p></li>
-        <li><img src={assets.nuts} className='w-20 h-20' alt="dates" /><p>Dates</p></li>
-        <li><img src={assets.nuts} className='w-20 h-20' alt="dryfruits" /><p>Dryfruits</p></li>
-        <li><img src={assets.nuts} className='w-20 h-20' alt="combo" /><p>Combo</p></li>
-      </ul>
-    </div>
+    <>
+      <div className="flex justify-center gap-10 ">
+        <div className='flex justify-between gap-5 bg-[#D79700] items-center px-10 py-2 my-5 rounded-[50px] text-black text-[20px] '>
+          <img src={assets.nuts} className='-ml-7 rounded-[50px] w-15 h-15' alt="all" />
+          <p>All</p>
+        </div>
+        <div className='flex justify-between gap-5 bg-[#D79700] items-center px-10 py-2 my-5 rounded-[50px] text-black text-[20px] '>
+          <img src={assets.nuts} className='-ml-7 rounded-[50px] w-15 h-15' alt="honey" />
+          <p>Honey</p>
+        </div>
+        <div className='flex justify-between gap-5 bg-[#D79700] items-center px-10 py-2 my-5 rounded-[50px] text-black text-[20px] '>
+          <img src={assets.nuts} className='-ml-7 rounded-[50px] w-15 h-15' alt="nuts" />
+          <p>Nuts</p>
+        </div>
+        <div className='flex justify-between gap-5 bg-[#D79700] items-center px-10 py-2 my-5 rounded-[50px] text-black text-[20px] '>
+          <img src={assets.nuts} className='-ml-7 rounded-[50px] w-15 h-15' alt="dates" />
+          <p>Dates</p>
+        </div>
+        <div className='flex justify-between gap-5 bg-[#D79700] items-center px-10 py-2 my-5 rounded-[50px] text-black text-[20px] '>
+          <img src={assets.nuts} className='-ml-7 rounded-[50px] w-15 h-15' alt="dryfruits" />
+          <p>Dryfruits</p>
+        </div>
+        <div className='flex justify-between gap-5 bg-[#D79700] items-center px-10 py-2 my-5 rounded-[50px] text-black text-[20px] '>
+          <img src={assets.nuts} className='-ml-7 rounded-[50px] w-15 h-15' alt="combo" />
+          <p>Combo</p>
+        </div>
+      </div>
+    </>
   )
 }
 
