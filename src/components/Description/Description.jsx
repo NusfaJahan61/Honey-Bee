cb295c4a697e1de6bb4dce4072835e61c3063d71
NusfaJahan61 <nusfaikra@gmail.com>
2026-02-02 20:17:18 +0600

    Product description modal v2

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,7 +1,11 @@
-import { useContext } from "react"
-import { item_list } from "../../assets/assets"
-import { assets } from "../../assets/assets"
-import { StoreContext } from "../../StoreContext/StoreContext"
+import { useContext } from "react";
+import { item_list } from "../../assets/assets";
+import { assets } from "../../assets/assets";
+import { StoreContext } from "../../StoreContext/StoreContext";
+import { FaTruck } from "react-icons/fa";
+import { SlCalender } from "react-icons/sl";
+import { MdVerified } from "react-icons/md";
+import { FaBangladeshiTakaSign } from "react-icons/fa6";
 
 const Description = () => {
 
@@ -14,36 +18,43 @@ const Description = () => {
         item_list.map((item, index) => {
           if (description === item.name) {
             return (
-              <div className="modal-overlay" key={index}>
-                <div className="bg-white m-auto mt-25 w-1/2 h-5/7 p-10 rounded-2xl">
-                  <img onClick={() => { setDescription(""); setShowDesc(false); enableScroll() }} src={assets.cross_icon} alt="close" />
-                  <div className="flex">
-                    <div className="flex-col">
+              <div className="modal-overlay backdrop-blur-sm" key={index}>
+                <div className="modal-content">
+                  <img className="flex ml-[97%] justify-end cursor-pointer" onClick={() => { setDescription(""); setShowDesc(false); enableScroll() }} src={assets.cross_icon} alt="close" />
+                  <div className="flex gap-5 justify-between">
+                    <div className="flex-col w-1/2 gap-5">
                       <div className="">
-                        <img className="w-100 h-100 rounded-2xl" src={item.image} alt="product image" />
+                        <img className=" rounded-2xl" src={item.image} alt="product image" />
                       </div>
-                      <div className="flex justify-between mt-10">
-                        <div className="h-20 w-20 border-2 border-[#494846] rounded-2xl ">250g</div>
-                        <div className="h-20 w-20 border-2 border-[#494846] rounded-2xl ">350g</div>
-                        <div className="h-20 w-20 border-2 border-[#494846] rounded-2xl ">500g</div>
-                        <div className="h-20 w-20 border-2 border-[#494846] rounded-2xl ">1000g</div>
+                      <p className="text-[18px] mt-5">Select Quantity</p>
+                      <div className="flex justify-between mt-5 w-4/5">
+                        <div className="amount-btn">250g</div>
+                        <div className="amount-btn">350g</div>
+                        <div className="amount-btn">500g</div>
+                        <div className="amount-btn">1000g</div>
                       </div>
                     </div>
                     {/* Product descriptions */}
-                    <div className="flex-col ml-10">
+                    <div className="w-[45%] flex-col">
                       <h1 className="text-3xl font-bold">{item.name}</h1>
-                      <p className="mt-5 text-lg">{item.description}</p>
-                      <h2 className="text-2xl font-semibold mt-5">₹ {item.price}</h2>
-                      <button className="bg-black text-white px-10 py-3 rounded-full mt-10 hover:bg-gray-800">Add to Cart</button>
-                      {/* {
-                        !cartsItem[id]
-                          ? <button onClick={() => addToCart(id)} className="cursor-pointer bg-[#f7d31e] text-[20px] text-center px-3.5 py-1 rounded-[50px]">+</button>
-                          : <div className=" bg-[#f7d31e] flex justify-between text-[20px] text-center gap-5 items-center px-5 py-1 rounded-[50px]">
-                            <button className="cursor-pointer " onClick={() => removeFromCart(id)}>-</button>
-                            <p className="text-[16px]">{cartsItem[id]}</p>
-                            <button className="cursor-pointer" onClick={() => addToCart(id)}>+</button>
-                          </div>
-                      } */}
+                      <div className="flex-col">
+                        <div className="">
+                          <p className="flex items-center gap-2 mt-5"><FaBangladeshiTakaSign />{item.price}</p>
+                        </div>
+                        <div className="">
+                          <p className="flex items-center gap-2 mt-5"><FaTruck />Delivery charge 80/-</p>
+                        </div>
+                        <div className="">
+                          <p className="flex items-center gap-2 mt-5"><SlCalender /> Free Delivery</p>
+                        </div>
+                        <div className="">
+                          <p className="flex items-center gap-2 mt-5"><MdVerified /> Free Delivery</p>
+                        </div>
+                      </div>
+                      <p>Quantity</p>
+                      <button className="bg-[#FFB900] text-black px-10 py-3 rounded-full my-5 w-full hover:bg-[#febc05d5]">Add to Cart</button>
+                      <p className="font-bold text-[18px]">Product Descriptions: </p>
+                      <p className=" ">{item.description}</p>
                     </div>
                   </div>
 
