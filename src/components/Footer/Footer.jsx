eee41793cc187c1d5daa4ad7fed7813df71bd407
NusfaJahan61 <nusfaikra@gmail.com>
2026-02-13 23:56:42 +0600

    Responsive footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,8 +9,8 @@ import { StoreContext } from "../../StoreContext/StoreContext";
 const Footer = () => {
   const { setNav } = useContext(StoreContext)
   return (
-    <div id="footer" className=" capitalize bg-[#D69604] flex flex-col  h-120 *:text-black ">
-      <div className="w-3/4 m-auto mb-10 mt-20 flex justify-between">
+    <div id="footer" className=" capitalize bg-[#D69604] flex flex-col  h-120 *:text-black  sm:grid-rows-3 sm:gap-3">
+      <div className="w-3/4 m-auto mb-10 mt-20 flex justify-between sm:grid-rows-2 sm:gap-5">
         <div className="">
           <Link to='/'><img onClick={() => setNav("Home")} className="w-40 " src={assets.logo} alt="" /></Link>
           <Link to='/contact_us'><button onClick={() => setNav("Contact us")} className=" px-9 py-2 text-zinc-900 rounded-md my-4 border border-black hover:border-transparent hover:bg-amber-300 active:bg-[#f5df1d] bg-transparent  duration-400 hover:cursor-pointer">send email</button></Link>
@@ -20,7 +20,7 @@ const Footer = () => {
             <a target="_blank" href="https://telegram.com/"><FaTelegram /></a>
           </div>
         </div>
-        <div className="flex gap-10 mt-5">
+        <div className="flex gap-10 mt-5" >
           <div className="w-60 flex flex-col gap-5 ">
             <Link to='/contact_us' ><h1 onClick={() => setNav("Contact us")} className="font-bold uppercase text-[18px] tracking-widest hover:cursor-pointer">contact us</h1></Link>
             <p className="text-[15px]">DFO Office, Sundarbans East Forest Division, Khulna, Bangladesh
@@ -34,7 +34,6 @@ const Footer = () => {
             <p>Nuts</p>
             <p>Dates</p>
             <p>Dryfruits</p>
-            <p>Combo</p>
           </div>
           <Link to='/about_us'><div onClick={() => setNav("About")} className="w-40 flex flex-col gap-3 *:hover:cursor-pointer">
             <h1 className="font-bold uppercase text-[18px] tracking-widest mb-2">company</h1>
